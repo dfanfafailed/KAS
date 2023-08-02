4c4ab2e4f296ff044b53a60a111734212b07bffc
faiskndr <faisaliskandar204@gmail.com>
2023-08-02 19:50:49 +0700

    modifed app/Http/Controllers/PengeluaranController.php

--- app/Http/Controllers/PengeluaranController.php
+++ app/Http/Controllers/PengeluaranController.php
@@ -2,16 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pembayaran;
 use App\Models\Pengeluaran;
 use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PengeluaranController extends Controller
 {
     public function index()
     {
-        $pengeluaran = Pengeluaran::all();
+        // $pengeluaran = Pengeluaran::all();
+        $pengeluaran = DB::table('pengeluaran')
+            ->leftJoin('pembayaran', 'pengeluaran.id', '=', 'pembayaran.id_pengeluaran')
+            ->leftJoin('users', 'pembayaran.id_siswa', '=', 'users.id')
+            ->select('pengeluaran.*', 'pembayaran.uang_kembali', 'users.name')
+            ->get();
+        // dd($pengeluaran);
+
         return view('pengeluaran.index', compact('pengeluaran'));
     }
 
@@ -25,7 +34,15 @@ class PengeluaranController extends Controller
 
     public function store(Request $request)
     {
-        Pengeluaran::create($request->except('_token'));
+        $pengeluaran = Pengeluaran::create($request->except('_token'));
+        if ($pengeluaran->id_siswa != NULL) {
+            Pembayaran::create([
+                'id_pengeluaran' => $pengeluaran->id,
+                'id_siswa' => $pengeluaran->id_siswa,
+                'tanggal' => $pengeluaran->tanggal,
+                'uang' => 0,
+            ]);
+        }
         return 'OK';
     }
 }
